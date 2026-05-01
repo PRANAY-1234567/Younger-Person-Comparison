e0ee9b648df3d6e12707f9a1e7fb22dab2efec2c
Pranay <pranayvjadhao197@gmail.com>
2026-05-01 21:46:11 +0530

    Initial commit

---
+++ Demo.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def getInfo(self):
+        print("Name   :", self.name)
+        print("Age    :", self.age)
+
+    def younger(self, t):
+        if self.age < t.age:
+            return self
+        else:
+            return t
+
+
+# SAME as Java
+p1 = Person("Akshay", 26)
+p2 = Person("Saurabh", 30)
+
+y = p1.younger(p2)
+
+print("Younger person")
+y.getInfo()
